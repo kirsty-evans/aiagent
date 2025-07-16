674a1adb3275e8a48801844c2963e26e31d6982d
kirsty-evans <kirstysevans@gmail.com>
2025-07-16 20:31:21 +1000

    function declaration (need to test)

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -34,5 +34,17 @@ def get_files_info(working_directory, directory=None):
         except:
             return f'Error: Cannot get directory contents for {abspath_full_path}'
 
-
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
 
--- main.py
+++ main.py
@@ -5,9 +5,19 @@ import sys
 from dotenv import load_dotenv 
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
 
 # system prompt instructs the AI how to behave and respond despite what the user prompt is
-system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
 # user enters their prompt as a command line argument - uv run main.py {prompt}
 user_prompt = sys.argv[1]
 
@@ -15,6 +25,13 @@ user_prompt = sys.argv[1]
 messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
 ]
+
+# list of all available functions
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
 
 # loads environment variables from .env
 load_dotenv()
@@ -26,11 +43,17 @@ client = genai.Client(api_key=api_key)
 # generate content method to get a response from AI
 response = client.models.generate_content(
     model='gemini-2.0-flash-001', contents=messages,
-	config=types.GenerateContentConfig(system_instruction=system_prompt),
+	config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
 )
 
-# generate_content returns an object, so convert to text to see the AI answer
-print(response.text)
+# prints the AI response
+# first checks if the AI response contains function calls
+if response.function_calls:
+	for function_call_part in response.function_calls:
+		print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+else:
+	# generate_content returns an object, so convert to text to see the AI answer
+	print(response.text)
 
 # prints number of tokens consumed by the interaction
 prompt_tokens = response.usage_metadata.prompt_token_count
@@ -41,3 +64,6 @@ if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
 	print(f"User prompt: {user_prompt}")
 	print(f"Prompt tokens: {prompt_tokens}")
 	print(f"Response tokens: {response_tokens}")
+
+
+
